a93dd60adb2916e3ccf5465faca8d6a4bdd5b84a
ericzakariasson <eric.zakariasson@gmail.com>
2023-02-27 18:01:55 -0700

    chore: add simple document list component

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -2,19 +2,22 @@ import { Sidebar } from "ui";
 import Header from "../components/Header";
 import { useAccount } from "wagmi";
 import { authenticateCompose } from "../lib/compose";
+import { DocumentList } from "../components/DocumentList";
 
 export default function Web() {
   const { isConnected } = useAccount();
+  const isAuthenticated =
+    typeof localStorage === "undefined" ? false : localStorage.getItem("did");
   return (
     <>
       <Header />
-      <Sidebar />
       <main className="pl-64">
         {isConnected && (
           <button onClick={() => authenticateCompose()} type="button">
             authenticate composedb
           </button>
         )}
+        {isAuthenticated && <DocumentList />}
       </main>
     </>
   );
