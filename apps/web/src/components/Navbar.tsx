88e12ebe683ea2958280385d4dd82459e2da999f
ericzakariasson <eric.zakariasson@gmail.com>
2023-03-01 17:24:39 -0700

    fix: layout

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -24,7 +24,11 @@ export const Navbar = () => {
                 className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700"
                 aria-current="page"
               >
-                <ConnectButton showBalance={false} chainStatus={"none"} />
+                <ConnectButton
+                  showBalance={false}
+                  chainStatus={"none"}
+                  accountStatus={"address"}
+                />
               </a>
             </li>
           </ul>
--- apps/web/src/pages/create.tsx
+++ apps/web/src/pages/create.tsx
@@ -2,11 +2,17 @@ import { NextPage } from "next/types";
 
 import { useState, useRef } from "react";
 import { useMutation } from "react-query";
+import { useAccount } from "wagmi";
 import { Editor } from "../components/Editor";
+import { authenticateCompose } from "../lib/compose";
 
 const CreatePage: NextPage = () => {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+
+  const { isConnected } = useAccount();
+  const isAuthenticated =
+    typeof localStorage === "undefined" ? false : localStorage.getItem("did");
 
   const { mutate, isLoading } = useMutation(async () => {
     await new Promise((resolve) => setTimeout(resolve, 2000));
@@ -20,6 +26,11 @@ const CreatePage: NextPage = () => {
 
   return (
     <form onSubmit={handleSubmit}>
+      {isConnected && !isAuthenticated && (
+        <button onClick={() => authenticateCompose()} type="button">
+          authenticate composedb
+        </button>
+      )}
       <input
         placeholder="Untitled"
         className="w-full text-6xl font-bold placeholder:text-gray-200"
