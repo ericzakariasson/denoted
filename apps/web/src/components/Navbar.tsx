4ca697b516c0a86e4daf4a1b175099327d3f442f
ericzakariasson <eric.zakariasson@gmail.com>
2023-03-02 23:45:15 -0700

    chore: update some styling

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useAccount, useConnect } from "wagmi";
 import { formatEthAddress } from "../utils/index";
 import { useDisconnect } from "wagmi";
@@ -8,30 +9,25 @@ export const Navbar = () => {
   const { disconnect } = useDisconnect();
 
   return (
-    <nav className="mx-auto max-w-3xl py-4 px-4">
+    <nav className="mx-auto max-w-3xl p-4">
       <div className="flex items-center justify-between">
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+        <Link href="/" className="font-semibold dark:text-white">
           denoted
-        </span>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="mt-4 flex flex-col items-center rounded-lg border border-gray-100 bg-gray-50 p-4 dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:text-sm md:font-medium md:dark:bg-gray-900">
-            <li>
-              {connectors.map((connector) => (
-                <button
-                  className="rounded-full border border-black px-2 py-0"
-                  key={connector?.id}
-                  onClick={() => connect({ connector })}
-                >
-                  {isConnected && address && (
-                    <p onClick={() => disconnect()}>
-                      {formatEthAddress(address, 5, 40)}
-                    </p>
-                  )}
-                  {!isConnected && <p>connect</p>}
-                </button>
-              ))}
-            </li>
-          </ul>
+        </Link>
+        <div>
+          {connectors.map((connector) => (
+            <button
+              className="rounded-full border border-black px-2 py-0"
+              key={connector?.id}
+              onClick={() =>
+                isConnected ? disconnect() : connect({ connector })
+              }
+            >
+              {isConnected && address
+                ? formatEthAddress(address, 5, 40)
+                : "connect"}
+            </button>
+          ))}
         </div>
       </div>
     </nav>
