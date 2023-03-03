17844d00daa6e5c1a01a034f128ad65dbfbc824f
0xOptimism <yannis@anotherblock.io>
2023-03-02 23:04:45 -0700

    fix: updated navbar design

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -1,9 +1,8 @@
 import { useAccount, useConnect } from "wagmi";
-
+import { formatEthAddress } from "../utils/index";
 export const Navbar = () => {
-  const { isConnected } = useAccount();
-  const { connect, connectors, error, isLoading, pendingConnector } =
-    useConnect();
+  const { isConnected, address } = useAccount();
+  const { connect, connectors } = useConnect();
 
   return (
     <nav className="mx-auto max-w-3xl py-4 px-4">
@@ -14,29 +13,18 @@ export const Navbar = () => {
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="mt-4 flex flex-col items-center rounded-lg border border-gray-100 bg-gray-50 p-4 dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:text-sm md:font-medium md:dark:bg-gray-900">
             <li>
-              <a
-                href="#"
-                className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700"
-                aria-current="page"
-              >
-                <div className="main">
-                  {connectors.map((connector) => (
-                    <button
-                      className="card"
-                      disabled={!connector?.ready}
-                      key={connector?.id}
-                      onClick={() => connect({ connector })}
-                    >
-                      {isConnected && <p>connected</p>}
-                      {!isConnected && <p>{connector?.name}</p>}
-                      {isLoading && connector.id === pendingConnector?.id && (
-                        <p>connecting...</p>
-                      )}
-                    </button>
-                  ))}
-                  {error && <div>{error.message}</div>}
-                </div>
-              </a>
+              {connectors.map((connector) => (
+                <button
+                  className="rounded-full border border-black px-2 py-0"
+                  key={connector?.id}
+                  onClick={() => connect({ connector })}
+                >
+                  {isConnected && address && (
+                    <p>{formatEthAddress(address, 5, 40)}</p>
+                  )}
+                  {!isConnected && <p>connect</p>}
+                </button>
+              ))}
             </li>
           </ul>
         </div>
