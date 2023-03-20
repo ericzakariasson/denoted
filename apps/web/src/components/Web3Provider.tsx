04afffd4095f690de4b879e14a5e92bae10180a6
ericzakariasson <ericz.zakariasson@gmail.com>
2023-03-20 21:02:53 +0100

    chore: remove web3auth

--- apps/web/src/components/Web3Provider.tsx
+++ apps/web/src/components/Web3Provider.tsx
@@ -3,14 +3,10 @@ import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { mainnet } from "wagmi/chains";
 import { infuraProvider } from "wagmi/providers/infura";
 
-import Web3AuthConnectorInstance from "../lib/web3auth";
-
 const { chains, provider } = configureChains([mainnet], [infuraProvider()]);
 
-// Set up client
 const client = createClient({
   autoConnect: true,
-  connectors: [Web3AuthConnectorInstance(chains as any)],
   provider,
 });
 
