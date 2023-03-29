b2df5f3ee7da3fc7b896309ed6fad158e72c5194
ericzakariasson <ericz.zakariasson@gmail.com>
2023-03-29 18:42:41 +0200

    chore: use explicit `isProductionRuntime` variable

--- apps/web/src/lib/analytics.ts
+++ apps/web/src/lib/analytics.ts
@@ -1,22 +1,24 @@
 import mixpanel from "mixpanel-browser";
 import { getBaseUrl } from "../utils/base-url";
-const isProduction = process.env.NODE_ENV === "production";
 
-if (isProduction) {
+// `NODE_ENV` will be `production` in Vercel Preview and Production environment. Set `NODE_ENV=production` if you want to test locally
+const isProductionRuntime = process.env.NODE_ENV === "production";
+
+if (isProductionRuntime) {
   mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_TOKEN as string, {
     api_host: `${getBaseUrl()}/mp`,
-    debug: !isProduction,
+    debug: !isProductionRuntime,
   });
 }
 
 export function track(event: string, properties?: Record<string, unknown>) {
-  if (isProduction) {
+  if (isProductionRuntime) {
     mixpanel.track(event, properties);
   }
 }
 
 export function page(url: string) {
-  if (isProduction) {
+  if (isProductionRuntime) {
     mixpanel.track("Page Viewed", {
       url,
     });
@@ -24,7 +26,7 @@ export function page(url: string) {
 }
 
 export function identify(identifier: string) {
-  if (isProduction) {
+  if (isProductionRuntime) {
     mixpanel.identify(identifier);
   }
 }
