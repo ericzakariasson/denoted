01576d255d63b6c0583b9bbe0a0117987f23f67f
Philip Andersson <philip.andersson20@gmail.com>
2023-04-05 08:32:34 +0200

    Feature/transaction command (#14)
    * implement data stack for tx
    * get erc20 transfer, value, fee, gas price and & block

--- apps/web/src/components/commands/commands.ts
+++ apps/web/src/components/commands/commands.ts
@@ -6,10 +6,11 @@ import { lensProfileCommand, lensPostCommand } from "./social/lens";
 import { netWorthCommand } from "./net-worth/command";
 import { nftFloorPriceCommand } from "./nft/command";
 import { tallyCommand } from "./tally/command";
+import { transactionCommand } from "./transaction/command";
 
 export const COMMANDS: CommandGroup[] = [
   { name: "Wallet", items: [balanceCommand, netWorthCommand] },
   { name: "Social", items: [lensProfileCommand, lensPostCommand] },
-  { name: "Other", items: [graphCommand, duneCommand, tallyCommand] },
+  { name: "Other", items: [graphCommand, duneCommand, tallyCommand, transactionCommand] },
   { name: "NFT", items: [nftFloorPriceCommand] },
 ];
