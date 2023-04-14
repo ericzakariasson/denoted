fbcad7efbc802d24afc90f3c986e39fd96a6e988
Philip Andersson <philip.andersson20@gmail.com>
2023-04-14 22:42:26 +0200

    use fileWriteStreamHandler

--- apps/web/src/pages/api/editor/upload.ts
+++ apps/web/src/pages/api/editor/upload.ts
@@ -1,68 +1,70 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { IncomingForm, Files, errors } from "formidable";
-import PinataSDK from '@pinata/sdk';
+import { IncomingForm } from "formidable";
+import { PassThrough } from "stream";
+import PinataSDK, { PinataPinResponse } from "@pinata/sdk";
+import * as Sentry from '@sentry/nextjs';
 
-const { FormidableError } = errors;
+type IpfsUpload = PinataPinResponse & { Filename: string };
+
+const pinata = new PinataSDK({
+    pinataApiKey: process.env.PINATA_API_KEY,
+    pinataSecretApiKey: process.env.PINATA_SECRET_API_KEY,
+});
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === "POST") {
-        const pinata = new PinataSDK({
-            pinataApiKey: process.env.PINATA_API_KEY,
-            pinataSecretApiKey: process.env.PINATA_SECRET_API_KEY,
+    if (req.method !== "POST") {
+        return res.status(405).json({ error: "Method not allowed" });
+    }
+
+    const uploadPromises: Promise<IpfsUpload>[] = [];
+
+    const form = new IncomingForm({
+        multiples: true,
+        keepExtensions: true,
+        allowEmptyFiles: false,
+        fileWriteStreamHandler: ({ newFilename, originalFilename }) => {
+            const pass = new PassThrough();
+
+            const promise = pinata.pinFileToIPFS(pass, {
+                pinataMetadata: {
+                    name: originalFilename,
+                },
+            }).then((results) : IpfsUpload => ({
+                ...results,
+                Filename: originalFilename ?? newFilename,
+            })).catch((error) => {
+                throw new Error(
+                    `Cannot upload ${originalFilename} file to IPFS`,
+                    { cause: error }
+                );
+            });
+
+            uploadPromises.push(promise);
+
+            return pass;
+        },
+    });
+
+    try {
+        await new Promise((resolve, reject) => {        
+            form.parse(req, (err, _, files) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(files);
+            });
         });
 
-        try {
-            const form = new IncomingForm({
-                multiples: true,
-                keepExtensions: true,
-                allowEmptyFiles: false,
-                uploadDir: __dirname // TODO: use fileWriteStreamHandler instead
-            });
+        const uploads = await Promise.all(uploadPromises);
 
-            const files: Files = await new Promise((resolve, reject) => {        
-                form.parse(req, (err, _, files) => {
-                    if (err) {
-                        return reject(err);
-                    }
-                    resolve(files);
-                });
-            });
-
-
-            try {
-                const ipfsResults = await Promise.all(Object.entries(files).map(async ([_, file]) => {
-                    // const options = {
-                    //     pinataMetadata: {
-                    //         name: 'Some image',
-                    //     },
-                    //     pinataOptions: {
-                    //         cidVersion: 0,
-                    //     }
-                    // };
-
-                    // TODO: check if what File[] actually implies
-                    if (Array.isArray(file)) { 
-                        return res.status(400).json({ error: 'More than 1 file' });
-                    }
-
-                    const result = await pinata.pinFromFS(file.filepath);
-        
-                    return result;
-                }));
-
-                return res.status(200).json({ message: "File uploaded successfully", data: ipfsResults });
-            } catch (error) {
-                return res.status(500).json({ error });
-            }
-        } catch (error) {
-            if (error instanceof FormidableError) {
-                return res.status(500).json({ error: error.message });
-            } else {
-                return res.status(500).json({ error: 'Internal Server Error' });
-            }
+        return res.status(200).json({ message: "File uploaded successfully", uploads });
+    } catch (error) {
+        if (error instanceof Error) {
+            Sentry.captureException(error);
+            return res.status(500).json({ error: error.message });
+        } else {
+            return res.status(500).json({ error: 'Internal Server Error' });
         }
-    } else {
-        return res.status(405).json({ error: "Method not allowed" });
     }
 }
 
