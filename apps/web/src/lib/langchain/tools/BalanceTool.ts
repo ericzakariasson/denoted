bb105557a473ab70d74283f2eb65f23a42b02e35
Philip Andersson <philip.andersson20@gmail.com>
2023-04-16 18:54:50 +0200

    disable returnDirect

--- apps/web/src/lib/langchain/tools/BalanceTool.ts
+++ apps/web/src/lib/langchain/tools/BalanceTool.ts
@@ -7,7 +7,6 @@ export class BalanceTool extends Tool {
 Use the balance tool to lookup the balance of some cryptocurrency or token, i.e. ETH, OP, USDC etc., for a specific wallet address (can also be an ENS name ending with '.eth') and blockchain.
 Please provide the input to the tool formatted as JSON containing address (string), chainId (number) & tokenSymbol (string).
     `;
-  returnDirect = true;
   async _call(arg: string) {
     const { address, chainId, tokenSymbol } = JSON.parse(arg);
     return JSON.stringify({
--- apps/web/src/lib/langchain/tools/NetWorthTool.ts
+++ apps/web/src/lib/langchain/tools/NetWorthTool.ts
@@ -7,7 +7,6 @@ export class NetWorthTool extends Tool {
 Use the net worth tool to lookup the total amount worth in USD of cryptocurrencies and tokens for a specific wallet address or ENS name and blockchain.
 Please provide the input to the tool formatted as JSON containing address (string) & chainId (number).
     `;
-  returnDirect = true;
   async _call(arg: string) {
     const { address, chainId } = JSON.parse(arg);
     return JSON.stringify({
