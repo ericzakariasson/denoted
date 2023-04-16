4d7e826b3f8b6efeda88e53d1ff26a430c7498cf
Philip Andersson <philip.andersson20@gmail.com>
2023-04-16 18:56:32 +0200

    use command name

--- apps/web/src/lib/langchain/tools/BalanceTool.ts
+++ apps/web/src/lib/langchain/tools/BalanceTool.ts
@@ -2,7 +2,7 @@ import { Tool } from "langchain/tools";
 import { balanceCommand } from "../../../components/commands/balance/command";
 
 export class BalanceTool extends Tool {
-  name = "balance";
+  name = balanceCommand.command;
   description = `
 Use the balance tool to lookup the balance of some cryptocurrency or token, i.e. ETH, OP, USDC etc., for a specific wallet address (can also be an ENS name ending with '.eth') and blockchain.
 Please provide the input to the tool formatted as JSON containing address (string), chainId (number) & tokenSymbol (string).
--- apps/web/src/lib/langchain/tools/NetWorthTool.ts
+++ apps/web/src/lib/langchain/tools/NetWorthTool.ts
@@ -2,7 +2,7 @@ import { Tool } from "langchain/tools";
 import { netWorthCommand } from "../../../components/commands/net-worth/command";
 
 export class NetWorthTool extends Tool {
-  name = "net-worth";
+  name = netWorthCommand.command;
   description = `
 Use the net worth tool to lookup the total amount worth in USD of cryptocurrencies and tokens for a specific wallet address or ENS name and blockchain.
 Please provide the input to the tool formatted as JSON containing address (string) & chainId (number).
