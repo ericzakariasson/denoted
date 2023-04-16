06fab9a9ad4f3330230c2a831629c75d35337aad
Kenny-Gin1 <kenny.chung@live.se>
2023-04-15 22:53:34 -0500

    feat: added some more placeholders

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -1,6 +1,5 @@
 import { cn } from "../utils/classnames";
-import { TwitterIcon } from "react-share";
-import { Profile, Theme } from "@lens-protocol/widgets-react";
+import { TwitterIcon, LinkedinIcon, EmailIcon } from "react-share";
 
 type FooterProps = {
   className?: string;
@@ -9,13 +8,27 @@ type FooterProps = {
 export function Footer({ className }: FooterProps) {
   return (
     <footer className={cn(className)}>
-      <div className="mb-5 flex flex-row justify-center items-center">
+      <div className="mb-5 flex flex-row justify-center items-center gap-5">
         <a
           href="https://twitter.com/denotedxyz"
           target="_blank"
           rel="noreferrer"
         >
-          <TwitterIcon size={32} round />
+          <TwitterIcon size={40} round />
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <LinkedinIcon size={40} round />
+        </a>
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <EmailIcon size={40} round />
         </a>
       </div>
       <p className="text-sm text-gray-500">
