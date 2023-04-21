fa57bd56e445306a82da852d77d31a8d9ff8fa7b
ericzakariasson <ericz.zakariasson@gmail.com>
2023-04-21 09:31:15 +0200

    refactor: config component

---
+++ apps/web/src/components/commands/ConfigForm.tsx
@@ -0,0 +1,113 @@
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+} from "../ui/select";
+
+import * as chains from "wagmi/chains";
+
+type Field =
+  | {
+      name: string;
+      type: "address";
+      defaultValue: string;
+    }
+  | {
+      name: string;
+      label: string;
+      type: "select";
+      options: string[];
+      defaultValue: string;
+    }
+  | {
+      name: string;
+      type: "chain";
+      defaultValue: string;
+    };
+
+type ConfigFormProps = {
+  fields: Field[];
+  onSubmit: (values: Record<string, FormDataEntryValue>) => void;
+};
+
+export function ConfigForm({ fields, onSubmit }: ConfigFormProps) {
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        onSubmit(Object.fromEntries(new FormData(event.currentTarget)));
+      }}
+      className="flex flex-col gap-4"
+    >
+      {fields.map((field) => {
+        switch (field.type) {
+          case "address": {
+            return (
+              <div className="grid gap-2">
+                <Label>Address</Label>
+                <Input
+                  name={field.name}
+                  placeholder="erci.eth or 0x123abc..."
+                  defaultValue={field.defaultValue ?? ""}
+                  required
+                />
+              </div>
+            );
+          }
+          case "select": {
+            return (
+              <div className="grid gap-2">
+                <Label>{field.label}</Label>
+                <Select
+                  name={field.name}
+                  defaultValue={field.defaultValue ?? ""}
+                >
+                  <SelectTrigger>
+                    <SelectValue />
+                  </SelectTrigger>
+                  <SelectContent className="w-full">
+                    {field.options.map((option) => (
+                      <SelectItem key={option} value={option}>
+                        {option.toUpperCase()}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
+            );
+          }
+          case "chain": {
+            return (
+              <div className="grid gap-2">
+                <Label>Chain</Label>
+                <Select name={field.name} defaultValue={field.defaultValue}>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Chain" />
+                  </SelectTrigger>
+                  <SelectContent className="w-full">
+                    {Object.values(chains)
+                      .filter((chain) => ("testnet" in chain ? false : true))
+                      .map((chain) => (
+                        <SelectItem
+                          key={chain.name}
+                          value={chain.id.toString()}
+                        >
+                          {chain.name}
+                        </SelectItem>
+                      ))}
+                  </SelectContent>
+                </Select>
+              </div>
+            );
+          }
+        }
+      })}
+      <Button type="submit">Save</Button>
+    </form>
+  );
+}
