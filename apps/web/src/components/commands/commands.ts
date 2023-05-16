2ea9a6eaf0ea2ced9e2bce72bca2aa87bd3cd034
ericzakariasson <eric.zakariasson@gmail.com>
2023-05-16 22:20:29 +0200

    feat: add bookmark

--- apps/web/src/components/commands/commands.ts
+++ apps/web/src/components/commands/commands.ts
@@ -4,15 +4,38 @@ import { duneCommand } from "./dune/command";
 import { graphCommand } from "./graph/command";
 import { lensProfileCommand, lensPostCommand } from "./social/lens";
 import { netWorthCommand } from "./net-worth/command";
-import { nftFloorPriceCommand, nftImageCommand, nftTotalSalesVolumeCommand, nftUniqueHoldersCommand } from "./nft/command";
+import {
+  nftFloorPriceCommand,
+  nftImageCommand,
+  nftTotalSalesVolumeCommand,
+  nftUniqueHoldersCommand,
+} from "./nft/command";
 import { tallyCommand } from "./tally/command";
 import { tokenPriceCommand } from "./tokens/command";
 import { transactionCommand } from "./transaction/command";
+import { bookmarkCommand } from "./bookmark/command";
 
 export const COMMANDS: CommandGroup[] = [
   { name: "Wallet", items: [balanceCommand, netWorthCommand] },
   { name: "Social", items: [lensProfileCommand, lensPostCommand] },
-  { name: "Other", items: [graphCommand, duneCommand, tallyCommand, transactionCommand] },
-  { name: "NFT", items: [nftFloorPriceCommand, nftImageCommand, nftTotalSalesVolumeCommand, nftUniqueHoldersCommand] },
-  { name: "Tokens", items: [tokenPriceCommand]}
+  {
+    name: "Other",
+    items: [
+      graphCommand,
+      duneCommand,
+      tallyCommand,
+      transactionCommand,
+      bookmarkCommand,
+    ],
+  },
+  {
+    name: "NFT",
+    items: [
+      nftFloorPriceCommand,
+      nftImageCommand,
+      nftTotalSalesVolumeCommand,
+      nftUniqueHoldersCommand,
+    ],
+  },
+  { name: "Tokens", items: [tokenPriceCommand] },
 ];
