664eae81c002e69abb39c680dc07d83b5d93cf36
Philip Andersson <philip.andersson20@gmail.com>
2023-05-23 14:37:05 +0200

    UI fix titles (#80)

--- apps/web/src/components/ui/card.tsx
+++ apps/web/src/components/ui/card.tsx
@@ -35,7 +35,7 @@ const CardTitle = React.forwardRef<
   <h3
     ref={ref}
     className={cn(
-      "text-lg font-semibold leading-none tracking-tight",
+      "text-lg font-semibold leading-none tracking-tight break-words",
       className
     )}
     {...props}
